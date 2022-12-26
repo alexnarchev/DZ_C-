1d1c80a6fa81078f391adb6038615c9c5eded9d7
Alex Narchev <alexnarchev@mail.ru>
2022-12-26 22:58:32 +0500

    add all

---
+++ D3_01_diapazon_koordinat_nomer_chetverti/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача №17. Напишите программу, которая принимает 
+// на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+// в которой находится эта точка.
+
+
+Console.WriteLine("Введите координаты точк по X");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точк по Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == 0 || y == 0)
+{
+    Console.WriteLine("Невозможно определить четверть");
+}
+
+else if (x > 0, y > 0)
+{
+    Console.WriteLine("Точка в 1-й четверти");
+}
+
+else if (x < 0, y > 0)
+{
+    Console.WriteLine("Точка в 2-й четверти");
+}
+
+else if (x < 0, y < 0)
+{
+    Console.WriteLine("Точка в 3-й четверти");
+}
+else if (x > 0, y < 0)
+{
+    Console.WriteLine("Точка в 4-й четверти");
+}
---
+++ D3_03_rasstoyanie_mejdu_dvumya_tochkami/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 20: Напишите программу, которая принимает 
+// на вход координаты двух точек и находит расстояние 
+// между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,10
+// A (7,-5); B (1,-1) -> 7,21
+
+
+Console.Write("Введите координаты х1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты х2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double res = Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2));
+Console.WriteLine($"Итог:{res:f2}");
