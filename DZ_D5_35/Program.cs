9c931f0870512eca3caeed0fc4411b33b6727fb7
Alex Narchev <alexnarchev@mail.ru>
2023-01-05 18:25:38 +0500

    seminar 5

--- DZ_D5_35/Program.cs
+++ DZ_D5_35/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задайте одномерный массив из 123 случайных чисел 
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел 
 // в диапазоне [0, 150]. Найдите количество элементов массива, 
 // значения которых лежат в отрезке [10,99].
 // Пример для массива из 5, а не 123 элементов. 
@@ -7,25 +7,28 @@
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
 
+int [] RandomArray(int length, int leftRange, int rightRange)
+{
+    int[] array = new int[length];
+    Random rand = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0, 150);
+    }
+    return array;
+}
 
-int Array = 123;
-int[] a = new int[Array];
-Random Random = new Random();
-
-for(int i = 0; i < a.Length; i++)
-a[i] = Random.Next(0, 100);
-
-for(int i = 0; i < a.Length; i++)
-System.Console.Write($"{a[i], 4}");
+for(int i = 0; i < array.Length; i++)
+Console.Write($"{array[i], 4}");
 
 int count = 0;
 
-for(var i = 0; i<a.Length; i++)
+for(var i = 0; i < array.Length; i++)
 {
     if(i > 10 && i < 99)
     count++;
 }
 
-System.Console.WriteLine();
-System.Console.WriteLine($"Значения элементов массива, которые лежат в отрезке [10,99]: {count}");
+Console.WriteLine();
+Console.WriteLine($"Значения элементов массива, которые лежат в отрезке [10,99]: {count}");
 
